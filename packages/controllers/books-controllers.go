f573e0b01bea6295d46066e29739f7e31b082165
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-24 06:32:17 +0530

    Created Routes Framework

--- packages/controllers/books-controllers.go
+++ packages/controllers/books-controllers.go
@@ -1,1 +1,11 @@
 package controllers
+
+import (
+	"net/http"
+)
+
+func CreateBook(w http.ResponseWriter, r *http.Request)  {}
+func GetBooks(w http.ResponseWriter, r *http.Request)    {}
+func GetBookById(w http.ResponseWriter, r *http.Request) {}
+func UpdateBook(w http.ResponseWriter, r *http.Request)  {}
+func DeleteBook(w http.ResponseWriter, r *http.Request)  {}
--- packages/routes/bookstore-routes.go
+++ packages/routes/bookstore-routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"book-management-api.io/packages/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
