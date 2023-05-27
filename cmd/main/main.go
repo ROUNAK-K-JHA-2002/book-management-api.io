6c71e5ed1e5965c9529d9e31de93cf0c8e2dfa1a
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-27 16:48:19 +0530

    Added Main.go file for accessing routes to server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+//this file will create the server and tell path of routes file
+
+import (
+	"log"
+	"net/http"
+
+	"book-management-api.io/packages/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()              //instance of Router
+	routes.RegisterBookStoreRoutes(r) //calling router package
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:1552", r)) //starting server
+}
