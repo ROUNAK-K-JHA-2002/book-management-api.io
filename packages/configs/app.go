752865a5c159727277789d6bffa6b2ca9e03cd64
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-27 18:04:05 +0530

    FIX mysql connection bug

--- packages/configs/app.go
+++ packages/configs/app.go
@@ -11,8 +11,10 @@ var (
 	db *gorm.DB //Making a variable Db pointing towards gorm.db
 )
 
+// user:password@/dbname?charset=utf8&parseTime=True&loc=Local
+
 func ConnectToDb() {
-	d, err := gorm.Open("mysql", "rounak@Rounak@00/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "rounak:Rounak@00@/LocalDB?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
