302dead5df6329a1919eb2f9935f9e769a5f7f56
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-05-27 17:03:05 +0530

    Completed functions to prepare DB query

--- packages/models/books.go
+++ packages/models/books.go
@@ -17,5 +17,31 @@ type Book struct {
 func init() {
 	configs.ConnectToDb() //Calling function from config
 	db = configs.GetDB()
-	db.AutoMigrate(&Book{}) // Send data to DB
+	db.AutoMigrate(&Book{}) //  auto migration for given models,
+}
+
+//Functions to talk to database
+
+func CreateBook(b *Book) *Book {
+	db.NewRecord(b) //Prepare SQL Query : Done by Gorm Package
+	db.Create(&b)   //Execute the query
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books) //Find all records of type Book
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
 }
